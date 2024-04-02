0a8b4a94309aadbadbb138f8abe9bd7fb6ad2d58
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-02 18:03:24 +0200

    finish

--- asset/js/main.js
+++ asset/js/main.js
@@ -23,6 +23,27 @@ createApp({
             console.log(i);
             if (i !== -1)
                 this.todolist.splice(i, 1)
+        },
+        addItem() {
+            let newObj = {
+                id: null, //generare id
+                text: this.itemText, // prendere valore
+                done: false
+            }
+            let nextId = 0;
+            this.todolist.forEach((el) => {
+                if (nextId < el.id) {
+                    nextId = el.id
+                }
+            })
+
+            newObj.id = nextId + 1;
+            //aggiungo in cima alla LISTA -> metodo array
+            //this.todolist.unshift(newObj);
+            this.todolist.push(newObj);
+            //ripulisco itemText
+            this.itemText = '';
+            console.log(this.todolist);
         }
 
     },
