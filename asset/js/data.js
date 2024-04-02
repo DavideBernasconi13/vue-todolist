eb07ecdbe4fdb9318e208e395e44221173374d6d
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-02 14:37:27 +0200

    init

---
+++ asset/js/data.js
@@ -0,0 +1,22 @@
+export const todolist = [
+    {
+        id: 1,
+        text: 'pane',
+        done: false
+    },
+    {
+        id: 2,
+        text: 'latte',
+        done: false
+    },
+    {
+        id: 3,
+        text: 'uova',
+        done: false
+    },
+    {
+        id: 4,
+        text: 'formaggio',
+        done: false
+    },
+]
---
+++ asset/js/main.js
@@ -0,0 +1,2 @@
+import { todolist } from "./data.js";
+
