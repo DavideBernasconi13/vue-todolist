dc9ed1ea08b075304da4789266d623968a635f49
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-02 15:20:36 +0200

    initialization Vue

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,2 +1,17 @@
 import { todolist } from "./data.js";
 
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            todolist
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+        console.log(todolist)
+    }
+}).mount('#app'); 
